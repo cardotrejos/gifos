0c64b4cf1bbdfaa434023e069d48780b3ba17232
Ricardo <cardotrejos@gmail.com>
2020-09-20 11:12:48 -0500

    adding assets, font awesome and global variables

--- scripts/script.js
+++ scripts/script.js
@@ -6,6 +6,26 @@ const urlTrending = `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&li
 
 const urlSearch = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${wordSearched}&limit=12&offset=0&rating=g&lang=en`;
 
+
+const createGifCard = () => {
+  let divCard = document.createElement('div')
+  let divIconDownload = document.createElement('div')
+  let divIconFavorite = document.createElement('div')
+  let divIconMaximize = document.createElement('div')
+  let link = document.createElement('a')
+  let img = document.createElement('img');
+  img.src = url;
+  divCard.className = "gif-card"
+  divIconDownload.className = "icon-dowload"
+  divIconFavorite.className = "icon-favorite"
+  divIconMaximize.className = "icon-maximize"
+  document.getElementById('searched-gifs').appendChild(divCard).appendChild(img);
+  document.getElementById('searched-gifs').appendChild(divCard).appendChild(divIconDownload)
+  document.getElementById('searched-gifs').appendChild(divCard).appendChild(divIconFavorite)
+  document.getElementById('searched-gifs').appendChild(divCard).appendChild(divIconMaximize)
+}
+
+
 // VARIABLES
 const searchInput = document.getElementById("search")
 const sugContainer = document.getElementById("sug-container")
@@ -17,16 +37,14 @@ async function getSearchedGifs() {
   return data;
 }
 
+console.log(getSearchedGifs())
+
 getSearchedGifs()
   .then(data => data.data.map(gif => gif.images.fixed_height.url)
     .forEach(url => {
-      let img = document.createElement('img');
-      img.src = url;
-      img.className = "gifs"
-      document.getElementById('searched-gifs').appendChild(img);
-    })
+        console.log(url)
+    }) 
   );
-
 
 async function getTrendingGifs() {
   const response = await fetch(urlTrending);
@@ -48,24 +66,24 @@ getTrendingGifs()
 
 searchInput.addEventListener('keyup', printAutocomplete);
 
-async function getAutocomplete(){
-    const response = await fetch(`https://api.giphy.com/v1/gifs/search/tags?api_key=${apiKey}&q=${searchInput.value.trim()}`)
-    .then(response=>response.json())
-    .then(content =>content.data)
-      .catch(err => {
-        console.error(err);
-      });
-    return response
+async function getAutocomplete() {
+  const response = await fetch(`https://api.giphy.com/v1/gifs/search/tags?api_key=${apiKey}&q=${searchInput.value.trim()}`)
+    .then(response => response.json())
+    .then(content => content.data)
+    .catch(err => {
+      console.error(err);
+    });
+  return response
 }
 
-async function printAutocomplete(){
-    sugContainer.innerHTML="";
-    const autoComplete = await getAutocomplete();
-    let arr = autoComplete.slice(0,5)
-    arr.forEach(suggText=>{
-        sugContainer.innerHTML+=
-    `
+async function printAutocomplete() {
+  sugContainer.innerHTML = "";
+  const autoComplete = await getAutocomplete();
+  let arr = autoComplete.slice(0, 5)
+  arr.forEach(suggText => {
+    sugContainer.innerHTML +=
+      `
     <div id="suggested-search" class="suggested-search"><span>${suggText.name}</span></div>
     `
-    })
+  })
 }
